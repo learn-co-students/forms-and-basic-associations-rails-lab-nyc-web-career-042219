4b6f6c34dc69e20a166347371084661877261449
gavinoconnor <gedmund@gmail.com>
2019-05-23 14:33:15 -0400

    Done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,10 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.new
+    @notes = Note.all
+    @artists = Artist.all
+
   end
 
   def create
@@ -47,7 +51,10 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(
+      :title,
+      :artist_name,
+      :genre_id,
+      note_contents: [])
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,35 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(genre)
+    self.genre = Genre.find_or_create_by(name: genre)
+  end
+
+  def genre_name
+    genre ? genre.name : nil
+  end
+
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(name: artist)
+  end
+
+  def artist_name
+    artist ? artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      unless content.strip == ''
+        notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    notes.map(&:content)
+  end
+
 end
